c21e820698e2a89d883311e4866d6aefa7d72c63
hari271002 <135507071+hari271002@users.noreply.github.com>
2024-07-15 17:47:55 +0530

    responsive is left

--- app.js
+++ app.js
@@ -3,13 +3,32 @@
 var tabDetails = document.getElementsByClassName('tab-details');
 var tabContents = document.getElementsByClassName('tab-contents');
 
-function openTab(type){
-    for(let tabDetail of tabDetails){
+function openTab(type) {
+    for (let tabDetail of tabDetails) {
         tabDetail.classList.remove('active-link');
     }
-    for(let tabContent of tabContents){
+    for (let tabContent of tabContents) {
         tabContent.classList.remove('active-tab');
     }
     event.currentTarget.classList.add('active-link');
     document.getElementById(type).classList.add('active-tab');
 }
+
+// const btn = document.querySelector('button')
+// const input1 = document.querySelector('.input_name')
+// const input2 = document.querySelector('.input_email')
+// const input3 = document.querySelector('.input_text')
+// btn.addEventListener('click', () => {
+//     input1.value = '';
+//     input2.value = '';
+//     input3.value = '';
+//     document.querySelector('#span').style.display = 'block';
+//     console.log('hello')
+// })
+// const sidemenu = document.getElementById('sidemenu');
+// function closeMenu() {
+//     sidemenu.style.right = "-300px";
+// }
+// function openMenu() {
+//     sidemenu.style.right = '0';
+// }
