f9b998ff78fc97af4810bd6bece5c0a443b60aa4
hari271002 <135507071+hari271002@users.noreply.github.com>
2024-07-17 12:34:45 +0530

    Revert "Merge branch 'main' of https://github.com/hari271002/personal-portfolio"
    This reverts commit 08099821e61b47501a9d8522492a2fec1c141f8b, reversing
    changes made to 3b128a1b1e41ab22b2cdd34a7e533a5b285e5564.

--- app.js
+++ app.js
@@ -14,3 +14,22 @@ function openTab(type) {
     event.currentTarget.classList.add('active-link');
     document.getElementById(type).classList.add('active-tab');
 }
+
+// const btn = document.querySelector('button')
+// const input1 = document.querySelector('.input_name')
+// const input2 = document.querySelector('.input_email')
+// const input3 = document.querySelector('.input_text')
+// btn.addEventListener('click', () => {
+//     input1.value = '';
+//     input2.value = '';
+//     input3.value = '';
+//     document.querySelector('#span').style.display = 'block';
+//     console.log('hello')
+// })
+// const sidemenu = document.getElementById('sidemenu');
+// function closeMenu() {
+//     sidemenu.style.right = "-300px";
+// }
+// function openMenu() {
+//     sidemenu.style.right = '0';
+// }
